fd13a98cfc7ee22cf4a5a02a64c0592f303f8529
Thibault Demars <thibault@derniercri.io>
2019-10-08 00:58:21 +0200

    first commit
    Unity project

---
+++ New Unity Project (1)/Assets/Sled.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sled : MonoBehaviour
+{
+    public float speed, maxSpeed = 100, rotation = 1f;
+
+    Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(Vector3.up * rotation * Input.GetAxis("Horizontal") * Time.deltaTime);
+        }
+
+        anim.SetFloat("rotation", Input.GetAxis("Horizontal"));
+    }
+}
